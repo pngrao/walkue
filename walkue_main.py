66ae70152d66d2b1832c77b67e4d7a7672ca6bd3
Pannaga Rao <pannaga.murthy@gmail.com>
2020-11-18 01:01:51 -0500

    Moved automatic turn off check to the end, formatted log messages

--- walkue_main.py
+++ walkue_main.py
@@ -5,7 +5,7 @@ import winsound
 
 preferences = {'interval' : 30, #in minutes test=0.25
               'auto_off' : True,
-              'auto_off_time' : 8} #in hours test=0.01
+              'auto_off_time' : 8} #in hours test=0.0083
 
 def main():
     """
@@ -17,7 +17,8 @@ def main():
     """
     print(f"Welcome {getpass.getuser()}, Walkue is here to help!")
     start_time = int(time.time())
-    print(f"Your walk cue will appear every {preferences['interval']} mins.")
+    print(f"{datetime.datetime.now():%I:%M:%S %p} : Starting Walkue.",
+          f"Expect walk cues every {preferences['interval']} mins")
     start_cue_timer(start_time, preferences)
 
 def start_cue_timer(primo_start_time, preferences):
@@ -37,27 +38,28 @@ def start_cue_timer(primo_start_time, preferences):
     while True:
         curr_time = int(time.time())
         try:
-            if preferences['auto_off'] and \
-                curr_time - primo_start_time >= auto_off_time:
-                print(f"Ok {getpass.getuser()}. Walkue's automatic turn off activated.")
-                print("Stopping Walkue. Take care!")
-                break
-        except KeyboardInterrupt:
-            print(f"Ok {getpass.getuser()}. You\'ve stopped Walkue.")
-            print("Take care!")
-            break
-
-        try:
             if curr_time - start_time >= interval:
-                print(f"{datetime.datetime.now():%I:%M:%S %p} : Walkue says WALK!")
+                print(f"{datetime.datetime.now():%I:%M:%S %p} : Walkue says, \'WALK!\'")
                 beep_frequency = 3020
                 beep_duration = 5000
                 winsound.Beep(beep_frequency, beep_duration)
                 start_time = curr_time
             time.sleep(1)
         except KeyboardInterrupt:
-            print(f"Ok {getpass.getuser()}. You\'ve stopped Walkue.")
-            print("Take care!")
+            print(f"{datetime.datetime.now():%I:%M:%S %p} : Ok {getpass.getuser()}. ",
+                  f"You\'ve stopped Walkue. Take care!")
+            break
+
+        try:
+            if preferences['auto_off'] and \
+                curr_time - primo_start_time >= auto_off_time:
+                print(f"{datetime.datetime.now():%I:%M:%S %p} : Ok {getpass.getuser()}.",
+                      f"Walkue's automatic turn off activated.",
+                      f"Stopping Walkue. Take care!")
+                break
+        except KeyboardInterrupt:
+            print(f"{datetime.datetime.now():%I:%M:%S %p} : Ok {getpass.getuser()}. ",
+                  f"You\'ve stopped Walkue. Take care!")
             break
 
 if __name__ == "__main__":
